4333a0edd3cdb19cc9e75e6fd5101ee49c0e58fb
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2023-04-16 14:31:59 +0300

    Update Operation.vue

--- src/components/tx/Operation.vue
+++ src/components/tx/Operation.vue
@@ -3,7 +3,13 @@
     v-if="typeof address === 'string'"
     class="card p-2 my-2 align-items-start"
   >
-    <Copy class="small" :text="removeAddressPrefix(address)" copy warp />
+    <Copy
+      class="small"
+      :url="`/address/${address}`"
+      :text="removeAddressPrefix(address)"
+      copy
+      warp
+    />
     <span class="my-1"> {{ satToBch(sat || "") }} BCH </span>
     <div v-if="tokenCategory && bcmrInfo" class="d-flex flex-wrap mt-2">
       <div
