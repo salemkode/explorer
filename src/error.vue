690535206d623bd4f6ede4247a85a8b8a0012fcf
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2023-05-12 05:35:03 +0300

    Support darkmode in error page.

--- src/error.vue
+++ src/error.vue
@@ -1,11 +1,8 @@
 <template>
   <Layout>
     <div class="m-auto p-md-5 p-sm-2 py-4 text-center">
-      <img
-        style="max-height: 300px; max-width: 85%"
-        src="~/assets/images/error.svg"
-      />
-      <h3 class="mt-5">
+      <div class="error-image m-auto" />
+      <h3 class="mt-2">
         <b>{{ errorMsg }}</b>
       </h3>
       <button
@@ -30,3 +27,18 @@ const errorMsg = computed(() => {
   return msg;
 });
 </script>
+
+<style scoped>
+.error-image {
+  height: 300px;
+  max-height: 300px;
+  max-width: 85%;
+  background: url("~/assets/images/error.svg") no-repeat center;
+  background-size: contain;
+  transition: background-image 0.3s ease-in-out;
+}
+
+[data-bs-theme="dark"] .error-image {
+  background-image: url("~/assets/images/error-dark.svg");
+}
+</style>
