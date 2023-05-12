5fb18b7d6f9b49d47bdc92544cf48c78449dc8f8
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2023-05-12 06:14:56 +0300

    Update utils.ts

--- src/module/utils.ts
+++ src/module/utils.ts
@@ -77,3 +77,29 @@ export const validateBcmrSchema = async (value: unknown) => {
     success: false,
   } as const;
 };
+
+const formatter = new Intl.RelativeTimeFormat(undefined, {
+  numeric: "auto",
+});
+
+const DIVISIONS = [
+  { amount: 60, name: "seconds" },
+  { amount: 60, name: "minutes" },
+  { amount: 24, name: "hours" },
+  { amount: 7, name: "days" },
+  { amount: 4.34524, name: "weeks" },
+  { amount: 12, name: "months" },
+  { amount: Number.POSITIVE_INFINITY, name: "years" },
+] as const;
+
+export const formatTimeAgo = (date: string) => {
+  let duration = (+new Date(date) - +new Date()) / 1000;
+
+  for (let i = 0; i < DIVISIONS.length; i++) {
+    const division = DIVISIONS[i];
+    if (Math.abs(duration) < division.amount) {
+      return formatter.format(Math.round(duration), division.name);
+    }
+    duration /= division.amount;
+  }
+};
