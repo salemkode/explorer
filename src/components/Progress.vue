addf701b1b3a071d5de4b12a264b99bb262577ed
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2023-05-12 06:21:57 +0300

    Support darkmode in progress.

--- src/components/Progress.vue
+++ src/components/Progress.vue
@@ -26,7 +26,7 @@
     <path
       class="tick"
       d="M6.5 10.5l2 2 5-5"
-      stroke="white"
+      stroke="transparent"
       stroke-width="2"
       stroke-linecap="round"
       fill="none"
@@ -61,43 +61,52 @@ setInterval(() => {
 }, 1000);
 </script>
 
-<style scoped>
-.stat-circle circle.bg {
-  fill: none;
-  stroke: #f1f1f1;
-  stroke-width: 2;
-  transition: 1s all;
-}
+<style lang="scss" scoped>
+.stat-circle {
+  circle {
+    transform: rotate(-90deg);
+    transform-origin: 50% 50%;
 
-.stat-circle.full circle.bg {
-  fill: #2ecc70;
-  stroke: none;
-}
+    &.bg {
+      fill: none;
+      stroke: var(--bs-border-color);
+      stroke-width: 2;
+      transition: 1s all;
+    }
 
-.stat-circle.full circle:is(.progress, .progress-hover) {
-  animation: full 1s;
-}
+    &:is(.progress, .progress-hover) {
+      fill: none;
+      stroke-width: 2;
+      stroke-dasharray: 51 51;
+      transition: 1s all;
+      stroke-linecap: round;
+    }
 
-.stat-circle circle {
-  transform: rotate(-90deg);
-  transform-origin: 50% 50%;
-}
+    &.progress {
+      stroke: #2ecc70;
+    }
 
-.stat-circle circle:is(.progress, .progress-hover) {
-  fill: none;
-  stroke-width: 2;
-  stroke-dasharray: 51 51;
-  transition: 1s all;
-  stroke-linecap: round;
-}
+    &.progress-hover {
+      transition: 1s stroke-dashoffset linear;
+      stroke: rgba(var(--bs-primary-rgb), 0.3);
+    }
+  }
 
-.stat-circle circle.progress {
-  stroke: #2ecc70;
-}
+  &.full {
+    .tick {
+      stroke: white;
+    }
+    circle {
+      &.bg {
+        fill: #2ecc70;
+        stroke: none;
+      }
 
-.stat-circle circle.progress-hover {
-  transition: 1s stroke-dashoffset linear;
-  stroke: #2ecc704d;
+      &:is(.progress, .progress-hover) {
+        animation: full 1s;
+      }
+    }
+  }
 }
 
 @keyframes full {
