694b9e9e9373f8045a3ea888b574d9fd64a21de4
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2023-05-16 06:07:52 +0300

    Add alert for chaingraph server.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,9 +3,15 @@
     <HomeHero />
     <div class="container">
       <HomeBlockChain />
+      <div class="alert alert-light my-3" role="alert">
+        The server of <a href="https://chaingraph.pat.mn">chaingraph.pat.mn</a>,
+        on which the project is being developed, has stopped. I use
+        <a href="https://demo.chaingraph.cash">demo.chaingraph.cash</a>
+        temporarily.
+      </div>
       <div class="home-content d-lg-grid my-3">
         <HomeUnconfirmedTransaction />
-        <div>
+        <div class="mt-2 mt-lg-0">
           <HomeBlocksList />
         </div>
       </div>
