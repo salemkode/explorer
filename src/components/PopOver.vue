2008caed5656370c642e451bfdfd3e08addf8155
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2023-07-31 20:45:33 +0300

    Use vue3-popper and remove @floating-ui/vue

--- src/components/PopOver.vue
+++ src/components/PopOver.vue
@@ -1,61 +1,28 @@
 <template>
-  <span
-    ref="reference"
-    class="reference"
-    @mouseover="() => !!props.msg && update()"
-  >
+  <PopperOver placement="auto-end" hover>
     <slot />
-  </span>
-  <div
-    ref="floating"
-    :style="{
-      position: strategy,
-      top: `${(y || 0) - 15}px`,
-      left: `${x || 0}px`,
-      width: '300px',
-    }"
-    class="bg-black p-1 px-2 rounded-1 text-white floating"
-  >
-    {{ props.msg }}
-  </div>
+    <template #content>
+      <div
+        class="bg-black p-1 px-2 rounded-1 text-white floating"
+        v-text="props.msg"
+      />
+    </template>
+  </PopperOver>
 </template>
 
 <script lang="ts" setup>
-import { autoUpdate, useFloating } from "@floating-ui/vue";
-
-const props = defineProps<{ msg: string }>();
-const reference = ref(null);
-const floating = ref(null);
-const { x, y, strategy, update } = useFloating(reference, floating, {
-  strategy: "fixed",
-  placement: "top",
-  whileElementsMounted: autoUpdate,
+const PopperOver = defineAsyncComponent({
+  loader: () => import("vue3-popper"),
 });
+const props = defineProps<{ msg: string }>();
 </script>
 
 <style>
 .floating {
-  display: none;
-  opacity: 0;
   transition: opacity 0.5s;
   font-size: 12.5px;
   margin-bottom: 5px;
   white-space: break-spaces;
-}
-
-.floating::before {
-  content: "";
-  position: absolute;
-  left: 50%;
-  top: 100%;
-  border: solid 10px transparent;
-  border-top-color: black;
-  transform: translate(-50%, 0);
-  border-radius: 1px;
-}
-
-.reference:hover ~ .floating {
-  display: block;
-  opacity: 1;
+  max-width: 300px;
 }
 </style>
