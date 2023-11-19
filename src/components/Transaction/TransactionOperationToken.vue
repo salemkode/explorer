5dfc00b70cbcd904aa9ca93dffea003d235698e0
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2023-11-19 05:42:13 +0300

    Hide unnecessary labels in transaction viewer

--- src/components/Transaction/TransactionOperationToken.vue
+++ src/components/Transaction/TransactionOperationToken.vue
@@ -2,12 +2,13 @@
 import { calculateDecimal } from "~/module/bitcoin";
 import { shortTx } from "~/module/utils";
 import { useRegistryStore } from "~/store";
+import type { Capability } from "~/types";
 type Maybe<T> = T | undefined | null;
 
 const registryStore = useRegistryStore();
 const props = defineProps<{
   tokenAmount: Maybe<string>;
-  tokenCapability: Maybe<string>;
+  tokenCapability: Maybe<Capability>;
   tokenCommitment: Maybe<string>;
   category: Maybe<string>;
 }>();
@@ -30,19 +31,20 @@ const metadata = computed(() => {
       <NuxtLink class="text-white" :to="`/token/${category}`">
         {{ shortTx(category) }}
       </NuxtLink>
-      <template v-if="amount">
-        <div class="mx-1">•</div>
-        {{ amount }}&nbsp;
-        <span>
-          {{ metadata?.symbol }}
-        </span>
-      </template>
+      <div class="mx-1">•</div>
+      {{ amount !== "0" ? amount : "" }}&nbsp;
+      <span>
+        {{ metadata?.symbol }}
+      </span>
     </div>
     <div v-if="tokenCommitment" class="badge text-bg-dark me-1 mb-1">
       NFT commitment:
       {{ tokenCommitment }}
     </div>
-    <div v-if="tokenCapability" class="badge text-bg-dark me-1 mb-1">
+    <div
+      v-if="tokenCapability && tokenCapability !== 'none'"
+      class="badge text-bg-dark me-1 mb-1"
+    >
       {{ tokenCapability }} nft
     </div>
   </div>
