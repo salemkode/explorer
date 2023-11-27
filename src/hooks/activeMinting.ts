2ec16d67255469ad48cede157d0732fb7feb8b0b
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2023-11-27 19:30:56 +0300

    Create custom hook to get more NFT info.

---
+++ src/hooks/activeMinting.ts
@@ -0,0 +1,26 @@
+import type { GetMintingTokenQuery } from "~/graphql/graphql";
+import type { bytea } from "~/types";
+
+export const GetMintingToken = gql`
+  query GetMintingToken($tokenCategory: bytea) {
+    output(
+      limit: 0
+      where: {
+        token_category: { _eq: $tokenCategory }
+        _and: { nonfungible_token_capability: { _eq: "minting" } }
+        _not: { spent_by: {} }
+      }
+    ) {
+      locking_bytecode
+    }
+  }
+`;
+
+export const useIsActiveMinting = (tokenCategory: bytea) => {
+  const { loading, result } = useQuery<GetMintingTokenQuery>(GetMintingToken, {
+    tokenCategory,
+  });
+
+  const isActiveMinting = computed(() => result.value?.output.length !== 0);
+  return { loading, isActiveMinting };
+};
---
+++ src/hooks/nftSupply.ts
@@ -0,0 +1,38 @@
+import type { GetSupplyNfTsQuery } from "~/graphql/graphql";
+import type { bytea } from "~/types";
+
+export const GetSupplyNFTs = gql`
+  query GetSupplyNFTs($offset: Int, $tokenCategory: bytea) {
+    output(
+      offset: $offset
+      where: {
+        token_category: { _eq: $tokenCategory }
+        _and: [{ nonfungible_token_capability: { _eq: "none" } }]
+        _not: { spent_by: {} }
+      }
+    ) {
+      locking_bytecode
+    }
+  }
+`;
+
+export const useNftSupply = (tokenCategory: bytea) => {
+  const supplyNFTs = ref(0);
+  const variables = reactive({
+    offset: 0,
+    tokenCategory,
+  });
+
+  const { loading, onResult } = useQuery<GetSupplyNfTsQuery>(
+    GetSupplyNFTs,
+    variables
+  );
+
+  onResult((response) => {
+    const length = response?.data?.output.length;
+    variables.offset += length === 5000 ? 5000 : 0;
+    supplyNFTs.value += length;
+  });
+
+  return { loading, supplyNFTs };
+};
