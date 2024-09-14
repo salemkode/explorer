0675af34a0f61494a25d3042cd6cda19a016f693
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2024-09-14 20:07:01 +0300

    Move electrum  declare module to custom file

--- src/module/electrum.ts
+++ src/module/electrum.ts
@@ -1,32 +1,4 @@
-import { ElectrumClient, type SubscribeCallback } from "electrum-cash";
-
-declare module "electrum-cash" {
-  type balance = { confirmed: number; unconfirmed: number };
-  type history = {
-    height: number;
-    tx_hash: string;
-  }[];
-
-  class ElectrumClient {
-    request(
-      method: "blockchain.address.get_balance",
-      address: string
-    ): Promise<Error | balance>;
-
-    request(
-      method: "blockchain.address.get_history",
-      address: string
-    ): Promise<Error | history>;
-
-    request<T>(method: string, ...parameters: string[]): Promise<Error | T>;
-
-    subscribe(
-      callback: SubscribeCallback,
-      method: string,
-      ...parameters: string[]
-    ): Promise<true>;
-  }
-}
+import { ElectrumClient } from "electrum-cash";
 
 export const electrum = new ElectrumClient(
   "mainnet-js-node",
---
+++ src/types/electrum.d.ts
@@ -0,0 +1,41 @@
+import type {
+	SubscribeCallback,
+} from "electrum-cash";
+
+declare module "electrum-cash" {
+	type balance = { confirmed: number; unconfirmed: number };
+	type history = {
+		height: number;
+		tx_hash: string;
+	}[];
+
+	export class ElectrumClient extends this.ElectrumClient {
+		constructor(
+			application: string,
+			version: string,
+			host: string,
+			port?: number,
+			scheme?: TransportScheme,
+			timeout?: number,
+			pingInterval?: number,
+		);
+
+		request(
+			method: "blockchain.address.get_balance",
+			address: string,
+		): Promise<Error | balance>;
+
+		request(
+			method: "blockchain.address.get_history",
+			address: string,
+		): Promise<Error | history>;
+
+		request<T>(method: string, ...parameters: string[]): Promise<Error | T>;
+
+		subscribe(
+			callback: SubscribeCallback,
+			method: string,
+			...parameters: string[]
+		): Promise<true>;
+	}
+}
