162ca67c7db86dcba32cd5a4cff01601be5ebad5
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2025-11-28 14:29:07 +0300

    update list

--- src/components/token/EmberAddress.vue
+++ src/components/token/EmberAddress.vue
@@ -20,75 +20,31 @@ import { addressToLockingBytecodeHex, calculateDecimal, removeAddressPrefix } fr
 import { GetTokenAddress } from "~/module/chaingraph";
 import { useStateStore } from "~/store";
 import type { tableColumn } from "~/types";
-
-const projects = [
-  {
-    "project": "Badger Survivors",
-    "address": "bitcoincash:zr2pasf2h8cecfj8st2qyht4hecwlc9f4ymp5l9dm7"
-  },
-  {
-    "project": "Coingraze",
-    "address": "bitcoincash:zr2zzu0wjzze67x24vkc358lnhpm5plj6vjeswfl08"
-  },
-  {
-    "project": "Crypto Focus Garden",
-    "address": 
-      "bitcoincash:zzzyq9g0n2ywatt0eajwre5h89aju3kk0svl94s4k9"
-    
-  },
-  {
-    "project": "Dance.Cash (kavinm)",
-    "address": "bitcoincash:qzw2pdwvc8p2x2acenet4l8k5ma704ly4qaa4cw23r"
-  },
-  {
-    "project": "bch-ipfs-scrape",
-    "address": "bitcoincash:zr7vzteyt0dflw75quhcldg0sdph30d08cmxlt835u"
-  },
-  {
-    "project": "Jump to Layer 1",
-    "address": "bitcoincash:zqk0qjl5p7fgndtsph494ul9ydjy0acv5c54wves4y"
-  },
-  {
-    "project": "FlowGuard",
-    "address": "bitcoincash:zpdah7k0pxmqu4sj8t0r4mjvxxwv4f0t7vvwchz4zh"
-  },
-  {
-    "project": "FortressVault",
-    "address": "bitcoincash:zqkxtz9m4pl3sqxvah7z689x9j07x5vzrvmv9ve90r"
-  },
-  {
-    "project": "Mean Reversion Portfolio Rebalancer",
-    "address": "bitcoincash:qp3jgmqxf9lrfhmqxzd0j7rrurccsic8agvquppaecx"
-  },
-  {
-    "project": "Advent Of Cash",
-    "address": "bitcoincash:zztcwdzny29vurzahmmlken9xuzvpj2yucmejnyamf"
-  },
-  {
-    "project": "Fun(d) Tokens",
-    "address": "bitcoincash:zpxq59t78fflnxxpe4v4v5cjgugt703rx5gsal96v2"
-  },
-  {
-    "project": "eventcollect.cash",
-    "address": "bitcoincash:zptlllye6aydcv7kndx4sr4vdfldv09ltyys40vrxzn"
-  },
-  {
-    "project": "EternalCash",
-    "address": "bitcoincash:zp7jqy9vprhaaezkew5cvk93enmnv325ngpftxhn0c"
-  },
-  {
-    "project": "USSD.Cash",
-    "address": "bitcoincash:zry267zd6qu2z9frxhq8q46j9d79v5y55cd1mck684"
-  },
-  {
-    "project": "BCH Game BattleCash",
-    "address": "bitcoincash:zqykhddZw6qjanvl7jr5khftss6mhy7s8qanpw3rxu"
-  },
-  {
-    "project": "Personas",
-    "address": "qpq8yv3w69474fjmcr2cw7qt729rklx0muzp2vs79j"
-  }
-]
+// Set up a list of projects to measure.
+const projects =
+[
+        { name: 'Badger Survivors',                    address: 'bitcoincash:zr2pasf2h8cecfj8st2qyht4hecwlc9f4ymp5l9dm7' },
+        { name: 'Coingraze',                           address: 'bitcoincash:zr2zzu0wjzze67x24vkc358lnhpm5plj6vjeswfl08' },
+        { name: 'Lamport',                             address: 'bitcoincash:zpwxwzqsn6zyhdr77hy34wh4ew2v3c0mk5kccddhgm' },
+        { name: 'Dance.Cash (1)',                      address: 'bitcoincash:qzsap449ca5a9vktj04a522l2kgrfqh975vgmjexzh' },
+        { name: 'Crypto Focus Garden',                 address: 'bitcoincash:zzzyq9g0n2ywatt0eajwre5h89aju3kk0svl94s4k9' },
+        { name: 'Dance.Cash (2)',                      address: 'bitcoincash:qzw2pdwvc8p2x2acenet4l8k5ma704ly4qaa4cw23r' },
+        { name: 'BCH Giveaway',                        address: 'bitcoincash:qp0yrpmemusnkzk07ngt790hg8cts7rm2q5lcau9r8' },
+        { name: 'bch-ipfs-scrape',                     address: 'bitcoincash:zr7vztevt0dflw75quhcldg0sdph30d08cmxlt835u' },
+        { name: 'Jump to Layer 1',                     address: 'bitcoincash:zqk0qjl5p7fgndtsph494ul9ydjy0acv5c54wves4y' },
+        { name: 'FlowGuard',                           address: 'bitcoincash:zpdah7k0pxmqu4sj8t0r4mjvxxwv4f0t7vvwchz4zh' },
+        { name: 'FortressVault',                       address: 'bitcoincash:qqkxtz9m4pl3sqxvah7z689x9j07x5vzrvuxkjhrss' },
+        { name: 'DancePulse',                          address: 'bitcoincash:qzanzk92dwq06s0guje3hlne6v9p4ckkhq98ycqg5z' },
+        { name: 'BCH Address Harmony',                 address: 'bitcoincash:zr3c5pd48dq4agyxswgqlkw7etl9vlls3ga0enwf4a' },
+        { name: 'Mean Reversion Portfolio Rebalancer', address: 'bitcoincash:zrg8xmhfqytlfmt7nwk44cymp2cv3y5kksr2gsyfyq' },
+        { name: 'Advent Of Cash',                      address: 'bitcoincash:zztcwdzny29vurzahmmlken9xuzvpj2yucmejnyamf' },
+        { name: 'Fun(d) Tokens',                       address: 'bitcoincash:zpxq59t78fflnxxpe4v4v5cjgugt703rx5gsal96v2' },
+        { name: 'eventcollect.cash',                   address: 'bitcoincash:zptlllye6aydcv7kndx4sr4vdfdv09ltyys40vrxzn' },
+        { name: 'BCH Game BattleCash',                 address: 'bitcoincash:zqykhddzw6qjanvl7jr5khftss6mhy7s8qanpw3rxu' },
+        { name: 'EternalCash',                         address: 'bitcoincash:zp7jqy9vprhaaezkew5cvk93enmnv325ngpftxhn0c' },
+        { name: 'Personas',                            address: 'bitcoincash:qpq8yv3w69474fjmcr2cw7qt729rklx0muzp2vs79j' },
+        { name: 'USSD.Cash',                           address: 'bitcoincash:zry267zd6qu2z9frxhq8q4ej9d79v5y55cdlmck684' },
+];
 
 const limit = ref(5000);
 const offset = ref(0);
@@ -132,7 +88,7 @@ const addressList = computed(() => {
 		);
 	}, new Map<`\\x${string}`, number>());
 
-	let items: tableColumn[][] = Array.from(
+	const foundedTokens: tableColumn[][] = Array.from(
 		lockingBytecode,
 		([lockingBytecode, amount]) => {
 			const address = stateStore.lockingBytecodeHexToCashAddress(
@@ -141,9 +97,13 @@ const addressList = computed(() => {
 			if (typeof address !== "string") {
 				return [];
 			}
+
+      if (!projects.find((project) => addressToLockingBytecodeHex(project.address) === lockingBytecode.substring(2))) {
+        return [];
+      }
 			return [
 				{
-					text: projects.find((project) => addressToLockingBytecodeHex(project.address) === lockingBytecode.substring(2))?.project || "",
+					text: projects.find((project) => addressToLockingBytecodeHex(project.address) === lockingBytecode.substring(2))?.name || "",
 				},
 				{
 					text: removeAddressPrefix(address),
@@ -158,10 +118,6 @@ const addressList = computed(() => {
 		},
 	).filter((item) => item.length > 0).filter((item) => item[0].text !== "");
 
-	if (items.length === limit.value) {
-		items = items.slice(0, -1);
-	}
-
-	return items;
+	return foundedTokens;
 });
 </script>
